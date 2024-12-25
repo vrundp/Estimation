c3a632e471e8aeb648c552caa718939228e6de21
Vrund Patel <vrundpatel@gmail.com>
2024-12-25 13:29:17 -0600

    implemented BaseEKF

--- ekf/ekf.cpp
+++ ekf/ekf.cpp
@@ -1,11 +1,5 @@
-
+// (c) 2024 Vrund Patel
 #include "ekf.h"
-
-#include <Eigen/Dense>
-#include <functional>
-
-using Eigen::MatrixXd;
-using Eigen::VectorXd;
 
 BaseEKF::BaseEKF()
 {
@@ -17,3 +11,133 @@ BaseEKF::~BaseEKF()
 
 }
 
+void BaseEKF::initialize(const VectorXd& x0, const MatrixXd& P0, const MatrixXd& D, const double t0, const double dt)
+{
+    x_ = x0;
+    P_ = P0;
+    D_ = D;
+    t_ = t0;
+    dt_ = dt;
+
+    nx_ = x0.size();
+    nv_ = D.row(0).size();
+}
+
+void BaseEKF::setControlInput(const VectorXd& u)
+{
+    u_ = u;
+
+    nu_ = u.size();
+}
+
+void BaseEKF::setProcessNoiseCovariance(const MatrixXd& Q)
+{
+    Q_ = Q;
+}
+
+void BaseEKF::setMeasurementNoiseCovariance(const MatrixXd& R)
+{
+    R_ = R;
+
+    nz_ = R.cols();
+}
+
+void BaseEKF::timeUpdate()
+{
+    // set the "initial conditions" for the integrator
+    VectorXd x0 = x_;
+    MatrixXd STM0 = MatrixXd::Identity(nx_, nx_);
+    MatrixXd L0 = MatrixXd::Zero(nx_, nv_);
+
+    // stack the "initial conditions" into a single vector
+    VectorXd X0(nx_ * (1 + nx_ + nv_));
+    X0 << x0, Eigen::Map<VectorXd>(STM0.data(), nx_ * nx_), Eigen::Map<VectorXd>(L0.data(), nx_ * nv_);
+
+    // propagate the state, STM, and L forward
+    std::pair<double, VectorXd> sol = integrator(f_full(t_, X0), X0, t_, dt_);
+
+    // extract time and update
+    t_ = sol.first;
+    // extract state, STM, and L stacked vector
+    VectorXd X = sol.second;
+
+    // extract STM
+    MatrixXd STM = Eigen::Map<MatrixXd>(X.segment(nx_, nx_ * nx_).data(), nx_, nx_);
+    // extract L
+    MatrixXd L = Eigen::Map<MatrixXd>(X.segment(nx_ * nx_, nx_ * nv_).data(), nx_, nv_);
+
+    // update state (now a priori state)
+    x_ = X.segment(0, nx_);
+    // update state covariance (now a priori covariance)
+    P_ = STM * P_ * STM.transpose() + L * Q_ * L.transpose();
+}
+
+void BaseEKF::measUpdate(const VectorXd& z)
+{
+
+    // computed measurement using the measurement model with a priori state
+    VectorXd zbar = h_meas(t_, x_);
+
+    // compute measurement Jacobian evaluated at a priori state
+    H_ = measJacobian(t_, x_);
+
+    // compute pre-fit measurement residual (observed - computed)
+    delz_ = z - zbar;
+
+    // compute innovation covariance matrix
+    S_ = H_ * P_ * H_.transpose() + R_;
+
+    // compute Kalman gain matrix
+    K_ = P_ * H_.transpose() * S_.inverse();
+
+    // update state (now a posteriori state)
+    x_ = x_ + K_ * delz_;
+    // update state covariance (now a posteriori covariance)
+    MatrixXd I = MatrixXd::Identity(nx_, nx_);
+    P_ = (I - K_ * H_) * P_ * (I - K_ * H_).transpose() + K_ * R_ * K_.transpose();
+}
+
+std::pair<double, VectorXd> BaseEKF::integrator(
+    std::function<VectorXd(const double, const VectorXd&)> f, 
+    const VectorXd& X, 
+    const double t, 
+    const double dt)
+{
+    VectorXd k1 = f(t, X);
+    VectorXd k2 = f(t + dt / 2, X + k1 * dt / 2);
+    VectorXd k3 = f(t + dt / 2, X + k2 * dt / 2);
+    VectorXd k4 = f(t + dt, X + k3 * dt);
+
+    VectorXd X_sol =  X + (k1 + 2 * k2 + 2 * k3 + k4) / 6 * dt;
+    double t_sol = t + dt;
+
+    return std::make_pair(t_sol, X_sol);
+}
+
+VectorXd BaseEKF::f_full(const double t, const VectorXd& X)
+{
+    // extract state vector
+    VectorXd x = X.segment(0, nx_);
+
+    // compute dynamics Jacobian at current state vector
+    A_ = dynJacobian(t, x);
+
+    // extract STM
+    MatrixXd STM = Eigen::Map<const MatrixXd>(X.segment(nx_, nx_ * nx_).data(), nx_, nx_);
+
+    // extract L
+    MatrixXd L = Eigen::Map<const MatrixXd>(X.segment(nx_ * nx_, nx_ * nv_).data(), nx_, nv_);
+
+    // compute STM time derivative
+    MatrixXd STM_dot = A_ * STM;
+    // compute process noise matrix time derivative
+    MatrixXd L_dot = A_ * L + D_;
+    // compute state time derivative
+    VectorXd x_dot = f_dyn(t, x);
+
+    // stack derivatives
+    VectorXd X_dot(nx_ * (1 + nx_ + nv_));
+    X_dot << x_dot, Eigen::Map<VectorXd>(STM_dot.data(), nx_ * nx_), Eigen::Map<VectorXd>(L_dot.data(), nx_ * nv_);
+
+    return X_dot;
+}
--- ekf/ekf.h
+++ ekf/ekf.h
@@ -1,3 +1,4 @@
+// (c) 2024 Vrund Patel
 #pragma once
 
 #include <Eigen/Dense>
@@ -7,9 +8,12 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 /*
-Base EKF
-system dynamics are modeled with non-additive noise
-measurements are modeled with additive noise
+Extended Kalman Filter
+dynamics model: dx/dt = f(t, x, u) + D * v(t)
+note: process noise in the dynamics function of the filter
+      is assumed to be zero mean; the model uncertainty is 
+      accounted for in the covariance based on D and Q
+measurement model: z = h(t, x) + w(t)
 */
 class BaseEKF
 {
@@ -18,22 +22,69 @@ public:
     BaseEKF();
     virtual ~BaseEKF();
 
-    void initialize(const VectorXd& x0, const MatrixXd& P0, const MatrixXd& D, const double t0);
+    /*
+    Initialize EKF with state and covariance along with process
+    noise mapping matrix, timestamp corresponding to initial state,
+    and filter update interval
+    */
+    void initialize(
+        const VectorXd& x0, 
+        const MatrixXd& P0, 
+        const MatrixXd& D,
+        const double t0, 
+        const double dt);
 
+    /*
+    Set a control input for the dynamics
+    note: derived class will need to use member variable in
+          dynamics model implementation if control input needed
+    */
+    void setControlInput(const VectorXd& u);
+
+    /*
+    Set the process noise covariance matrix
+    note: usually a diagonal matrix
+    */
     void setProcessNoiseCovariance(const MatrixXd& Q);
 
+    /*
+    Set the measurement noise covariance matrix
+    note: usually a diagonal matrix
+    */
     void setMeasurementNoiseCovariance(const MatrixXd& R);
 
+    /*
+    Compute the a priori state and covariance
+    */
     void timeUpdate();
 
+    /*
+    Compute the a posteriori state and covariance by processing 
+    raw measurement (observation)
+    */
     void measUpdate(const VectorXd& z);
 
-private:
+protected:
 
+    // dimension of state
+    unsigned int nx_;
+    // dimension of control input
+    unsigned int nu_;
+    // dimension of process noise
+    unsigned int nv_;
+    // dimension of measurement
+    unsigned int nz_;
+
+    // time
+    double t_;
+    // filter update interval
+    double dt_;
     // state vector
     VectorXd x_;
     // state covariance matrix
     MatrixXd P_;
+    // control input
+    VectorXd u_;
     // process noise covariance matrix
     MatrixXd Q_;
     // measurement noise covariance matrix
@@ -42,48 +93,58 @@ private:
     MatrixXd A_;
     // measurement Jacobian matrix
     MatrixXd H_;
+    // process noise mapping matrix
+    MatrixXd D_;
     // linearized state transition matrix
     MatrixXd STM_;
     // linearized process noise matrix
-    MatrixXd Gamma_;
+    MatrixXd L_;
+    // pre-fit measurement residual (innovation)
+    VectorXd delz_;
+    // innovation covariance matrix
+    MatrixXd S_;
+    // Kalman gain matrix
+    MatrixXd K_;
 
+private:
+
+    /*
+    Dynamics function that computes the time derivative of state vector, STM, and L
+    */
+    VectorXd f_full(const double t, const VectorXd& X);
+    
     /* 
     Pure virtual function that computes the non-linear dynamics function of the system
     as dx/dt = f(x, u, t) where x is the state, u is control input, t is the time
     */
-    virtual VectorXd f_dyn(const VectorXd& x, const VectorXd& u, const double t) = 0;
+    virtual VectorXd f_dyn(const double t, const VectorXd& x) = 0;
 
     /*
     Pure virtual function that computes the Jacobian of the system dynamics
-    as A = df/dx
+    as A = df/dx evaluated at the a posteriori state (before time update)
     */
-    virtual MatrixXd dynJacobian(const VectorXd& x, const VectorXd& u) = 0;
+    virtual MatrixXd dynJacobian(const double t, const VectorXd& x) = 0;
 
     /*
     Pure virtual function that computes the non-linear measurement function
-    as z = h(x) where x is the state
+    as z = h(x, t) where x is the state and t is the time
     */
-    virtual VectorXd h_meas(const VectorXd& x) = 0;
+    virtual VectorXd h_meas(const double t, const VectorXd& x) = 0;
 
     /*
     Pure virtual function that computes the Jacobian of the measurement function
-    as H = dh/dx
+    as H = dh/dx evaluated at the a priori state (after time update, before measurement update)
     */
-    virtual MatrixXd measJacobian(const VectorXd& x) = 0;
+    virtual MatrixXd measJacobian(const double t, const VectorXd& x) = 0;
 
     /*
-    Pure virtual function that implements an integrator that solves a differential
-    equation of the form dX/dt = f(X, u, t) as X = integral of f(X, u , t') dt' from t to t + dt
-    where X is some vector quantity that needs to be solved for, u is the control input, t is the time
+    Virtual function that implements an integrator that solves a differential
+    equation of the form dX/dt = f(t, X) as X = integral of f(t', X) dt' from t to t + dt
+    where X is some vector quantity that needs to be solved for and t is the time
     */
-    virtual VectorXd integrator(
-        std::function<VectorXd(const VectorXd&, const VectorXd&,  double)> f, 
+    virtual std::pair<double, VectorXd> integrator(
+        std::function<VectorXd(const double, const VectorXd&)> f, 
         const VectorXd& X, 
         const double t, 
-        const double dt) = 0;
-
-    /*
-    Function the propagates a generic state vector from t to t + dt using the integrator function
-    */
-    VectorXd propagate(const VectorXd& X, const double t, const double dt);
+        const double dt);
 };
